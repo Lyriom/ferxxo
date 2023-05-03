1430721139b54b5fad13ba211cf39169b156c085
josue riera <josueriera23@hotmail.com>
2023-05-02 21:18:33 -0500

    f

---
+++ c.c
@@ -0,0 +1,23 @@
+//Construya un programa que sume 2 números ingresados por el usuario, aplicando el //concepto de funciones.
+
+#include<stdio.h>
+
+int sumar(int a, int b)
+{
+    int resultado = a + b;
+    return resultado;
+}
+
+int main(int argc, char const *argv[])
+{
+    int num1, num2;
+    printf("Este programa suma 2 números\n");
+    printf("Ingrese el 1er número: ");
+    scanf("%d", &num1);
+    printf("Ingrese el 2do número: ");
+    scanf("%d", &num2);
+    int res = sumar(num1, num2);
+   
+    printf("el resultado de la suma es: %d", res);
+    return 0;
+}
